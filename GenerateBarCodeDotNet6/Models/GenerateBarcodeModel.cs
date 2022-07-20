5cc71af16e3fcf60472d4bdca4f10bb093c71cea
mdabusayedislam <abusayediit@gmail.com>
2022-07-20 14:00:51 +0600

    Add project files.

---
+++ GenerateBarCodeDotNet6/Models/GenerateBarcodeModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GenerateBarCodeDotNet6.Models
+{
+    public class GenerateBarcodeModel
+    {
+
+        [Display(Name = "Enter Barcode Text")]
+        public string BarcodeText
+        {
+            get;
+            set;
+        }
+    }
+}
